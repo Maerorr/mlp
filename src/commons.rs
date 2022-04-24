82671e1ec889ba71b6c77177f6a119bae44a9f9e
Maerorr <7hubert7@gmail.com>
2022-04-24 12:32:42 +0200

    feat: init with some functions

---
+++ src/commons.rs
@@ -0,0 +1,20 @@
+use std::f64::consts::E;
+
+use rand::{self, Rng};
+
+pub fn gen_weights(size: usize) -> Vec<f64> {
+    let mut rng = rand::thread_rng();
+    let mut weights: Vec<f64> = Vec::with_capacity(size);
+    for _ in 0..size {
+        weights.push(rng.gen::<f64>() * 2. - 1.); // scale to <-1, 1>
+    }
+    weights
+}
+
+pub fn sigmoid(x: f64) -> f64 {
+    1. / (1. + E.powf(-x))
+}
+
+pub fn der_sigmoid(x: f64) -> f64 {
+    E.powf(-x)/(1. + E.powf(-x)).powf(2.)
+}
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use commons::*;
+
+mod commons;
+
+struct Parameters {
+    in_neurons: usize,
+    hidden_neurons: usize,
+    out_neurons: usize,
+
+}
+
+fn main() {
+    
+}
